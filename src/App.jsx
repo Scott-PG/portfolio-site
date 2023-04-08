363bf4d4996148b38d54d996124aa5aca6547114
ScottPG <61914525+Scott-PG@users.noreply.github.com>
2023-04-08 16:02:31 -0400

    Site update March 2023 (#18)
    * Easy cleanups
    
    * Update to React 18.2
    
    * Clean up typing
    
    * Remove unused value
    
    * Remove Styled Components
    
    * CSS improvements
    
    * Move from CRA to Vite

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDarkMode } from "./Theme/useDarkMode";
+import "./App.css";
+
+import Header from "./Components/Header";
+import About from "./Components/About";
+import Showcase from "./Components/Showcase";
+import Presence from "./Components/Presence";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+
+  if (!componentMounted) {
+    return <div />;
+  }
+  return (
+    <div className={`app ${theme}`}>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <main>
+        <About />
+        <Showcase />
+        <Presence />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
--- src/Theme/Toggle.jsx
+++ src/Theme/Toggle.jsx
@@ -1,30 +1,21 @@
 import React from "react";
-import { func, string } from "prop-types";
-import ToggleContainer from "./Toggle.styled.jsx";
+
+import './Toggle.css'
 
 //credit to https://www.flaticon.com
 import { ReactComponent as MoonIcon } from "../Assets/icons/moon.svg";
 import { ReactComponent as SunIcon } from "../Assets/icons/sun.svg";
 
-const Toggle = ({ theme, toggleTheme }) => {
-  const isLight = theme === "light";
-  return (
-    <ToggleContainer
-      aria-hidden="true"
-      alt=""
-      lightTheme={isLight}
-      onClick={toggleTheme}
-    >
-      <SunIcon aria-hidden="true" alt="" tabIndex="-1" />
-      <MoonIcon aria-hidden="true" alt="" tabIndex="-1" />
-    </ToggleContainer>
-  );
-};
+const Toggle = ({ theme, toggleTheme }) => (
+  <button className={`toggle ${theme}`}
+    title="Dark Mode Toggle"
+    onClick={toggleTheme}
+  >
+    <SunIcon aria-hidden={true} alt="" className="sun" />
+    <MoonIcon aria-hidden={true} alt="" className="moon" />
+  </button>
+);
 
-Toggle.propTypes = {
-  theme: string.isRequired,
-  toggleTheme: func.isRequired,
-};
 
 export default Toggle;
 
