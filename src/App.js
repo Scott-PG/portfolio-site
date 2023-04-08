deb22fc717aaf9db2a49849701ee99175a3f560b
ScottPG <61914525+Scott-PG@users.noreply.github.com>
2023-04-08 13:27:02 -0400

    Remove Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
 import { useDarkMode } from "./Theme/useDarkMode";
-import { lightTheme, darkTheme } from "./Theme/theme";
-import { GlobalStyles } from "./Theme/global";
 import "./App.css";
 
 import Header from "./Components/Header";
@@ -14,24 +11,19 @@ import Footer from "./Components/Footer";
 const App = () => {
   const [theme, toggleTheme, componentMounted] = useDarkMode();
 
-  const themeMode = theme === "dark" ? darkTheme : lightTheme;
-
   if (!componentMounted) {
     return <div />;
   }
   return (
-    <ThemeProvider theme={themeMode}>
-      <>
-        <GlobalStyles />
-        <Header theme={theme} toggleTheme={toggleTheme} />
-        <main>
-          <About />
-          <Showcase />
-          <Presence />
-        </main>
-        <Footer />
-      </>
-    </ThemeProvider>
+    <div className={`app ${theme}`}>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <main>
+        <About />
+        <Showcase />
+        <Presence />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
