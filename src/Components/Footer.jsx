c6f315eab7222a7ebf04da6e6cd6d96ad8c49fff
ScottPG <61914525+Scott-PG@users.noreply.github.com>
2023-04-08 13:59:35 -0400

    CSS improvements

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import "./Footer.css";
+import './Footer.css'
 
-const Footer = () => {
-  return (
-    <footer>
-      <div className="footer-links">
-        <p>
-          <a href="https://reactjs.org/">Built in React</a>
-        </p>
-        <p>
-          <a href="https://github.com/Scott-PG/portfolio-site">
-            {"\u00a9"} 2020 Scott Griffith
-          </a>
-        </p>
-      </div>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer>
+    <div className="footer-links">
+      <a href="https://reactjs.org/">Built in React</a>
+      <a href="https://github.com/Scott-PG/portfolio-site">
+        {"\u00a9"} 2023 Scott Griffith
+      </a>
+    </div>
+  </footer>
+);
 
 export default Footer;
