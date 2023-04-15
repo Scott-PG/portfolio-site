a0c3c4d185aea75819bc5ad14c515f326cc852e8
ScottPG <61914525+Scott-PG@users.noreply.github.com>
2023-04-15 13:33:47 -0400

    implement stylelint and lint css

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,35 +1,33 @@
 import "./Showcase.css";
 import { SHOWCASE_ARRAY } from "./constants";
 
-const Showcase = () => {
-  return (
-    <div className="showcase">
-      <h3 id="showcase" className="orange">
-        Showcase
-      </h3>
-      {SHOWCASE_ARRAY.map((showcase) => (
-        <div key={showcase.title} className="portfolio-div">
-          <h4 className="portfolio-title">{showcase.title}</h4>
-          <img
-            className="portfolio-image"
-            width="400px"
-            height="294px"
-            src={showcase.imgSrc}
-            alt=""
-          />
-          <p className="portfolio-text">{showcase.description}</p>
-          <div className="portfolio-links">
-            <a href={showcase.deployedLink} className="portfolio-link">
-              Deployed Site
-            </a>
-            <a href={showcase.githubLink} className="portfolio-link">
-              GitHub Repository
-            </a>
-          </div>
+const Showcase = () => (
+  <div className="showcase">
+    <h3 id="showcase" className="orange">
+      Showcase
+    </h3>
+    {SHOWCASE_ARRAY.map((showcase) => (
+      <div key={showcase.title} className="portfolio-div">
+        <h4 className="portfolio-title">{showcase.title}</h4>
+        <img
+          className="portfolio-image"
+          width="400px"
+          height="294px"
+          src={showcase.imgSrc}
+          alt=""
+        />
+        <p className="portfolio-text">{showcase.description}</p>
+        <div className="portfolio-links">
+          <a href={showcase.deployedLink} className="portfolio-link">
+            Deployed Site
+          </a>
+          <a href={showcase.githubLink} className="portfolio-link">
+            GitHub Repository
+          </a>
         </div>
-      ))}
-    </div>
-  );
-};
+      </div>
+    ))}
+  </div>
+);
 
 export default Showcase;
