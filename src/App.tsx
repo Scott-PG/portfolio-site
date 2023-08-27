40ea88514a905a10e9842cf780eccbc86866c8b9
ScottPG <61914525+Scott-PG@users.noreply.github.com>
2023-08-27 18:43:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useDarkMode } from "./utils/useDarkMode";
 import "./App.css";
 
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
